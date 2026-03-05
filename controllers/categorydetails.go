d7fcc3290cad7c9fbd41d755efc2fbf14e815d8b
Ali-ahsan35 <syedahsan748@gmail.com>
2026-03-05 15:55:15 +0600

    new api created

---
+++ controllers/categorydetails.go
@@ -0,0 +1,69 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+type CategoryDetailsController struct {
+    beego.Controller
+}
+
+func (c *CategoryDetailsController) Get() {
+    country := strings.ToLower(c.Ctx.Input.Param(":country"))
+    if country == "" {
+        c.Data["json"] = map[string]string{"error": "country is required"}
+        c.ServeJSON()
+        return
+    }
+
+    apiURL := "https://presto:TRAV3LA1@ownerdirect.beta.123presto.com/api/v1/category/details/" + country +
+        "?aggsAvgPrice=1" +
+        "&aggsAvgRating=1" +
+        "&aggsAvgRoomSize=1" +
+        "&aggsCategory=1" +
+        "&device=desktop" +
+        "&items=1" +
+        "&locations=US" +
+        "&sections=1"
+
+    fmt.Println("CategoryDetails URL:", apiURL)
+
+    req, err := http.NewRequest("GET", apiURL, nil)
+    if err != nil {
+        fmt.Println("Request create error:", err)
+        c.Data["json"] = map[string]string{"error": err.Error()}
+        c.ServeJSON()
+        return
+    }
+
+    // Set required headers
+    req.Header.Set("Origin", "123presto-MS-ROW.com")
+    req.Header.Set("User-Agent", "desktop")
+    req.Header.Set("X-Api-key", "sparrowxkey@w3")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Request error:", err)
+        c.Data["json"] = map[string]string{"error": err.Error()}
+        c.ServeJSON()
+        return
+    }
+    defer resp.Body.Close()
+
+    bodyBytes, _ := io.ReadAll(resp.Body)
+    fmt.Println("Status:", resp.StatusCode)
+    fmt.Println("Response:", string(bodyBytes[:min(len(bodyBytes), 500)]))
+
+    var result map[string]interface{}
+    json.Unmarshal(bodyBytes, &result)
+
+    c.Data["json"] = result
+    c.ServeJSON()
+}
